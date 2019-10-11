8e4e4970bfc1b8a115fccc12d8feeb82dd3605c8
Evi <evicoach@gmail.com>
2019-10-11 09:18:14 +0100

    Added screens and widget folders to separate widgets used as screens from other widgets used within a screen

---
+++ meals_app/lib/screens/category_meals_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import '../widgets/meal_item.dart';
+
+import '../dummy_data.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  static const routeName = '/category-meals';
+  @override
+  Widget build(BuildContext context) {
+    final routesArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final categoryTitle = routesArgs['title'];
+    final categoryId = routesArgs['id'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(
+          categoryId); // only those that have the same category id as this id
+    }).toList();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(categoryTitle),
+      ),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          // return a function which is built as a single row
+          return MealItem(
+            title: categoryMeals[index].title,
+            imageUrl: categoryMeals[index].imageUrl,
+            affordability: categoryMeals[index].affordability,
+            complexity: categoryMeals[index].complexity,
+            duration: categoryMeals[index].duration,
+          );
+        },
+        itemCount: categoryMeals.length,
+      ),
+    );
+  }
+}
